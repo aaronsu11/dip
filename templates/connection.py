2585132992095e7e16f0437cc3da9fa325f4b3bf
Aaron <aaronsu11@outlook.com>
2020-03-21 00:30:07 +1100

    edited connection template

--- templates/connection.py
+++ templates/connection.py
@@ -13,6 +13,14 @@ def connectVehicle():
     args = parser.parse_args()
     connection_string = args.connect
 
+    if not connection_string:
+        print "No connection string provided. Start simulator (SITL)"
+        import dronekit_sitl
+        sitl = dronekit_sitl.start_default()
+        connection_string = sitl.connection_string()
+
+    # Connect to the Vehicle.
+    print("Connecting to vehicle on: %s" % (connection_string,))
     vehicle = connect(connection_string, wait_ready=True)
 
     return vehicle
